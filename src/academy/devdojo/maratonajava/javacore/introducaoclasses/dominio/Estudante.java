1c7afdca32ff5af2bd0bb31eb9d98d3c36d6c7f5
Bruno Tinetti Ribeiro <ra139256@uem.br>
2026-01-13 11:23:19 -0300

    Adicionado novo pacote "javacore"
    Introdução à orientação à objetos em java

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.dominio;
+
+public class Estudante {
+    public String name;
+    public int age;
+    public char gender;
+}
---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTeste01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.name = "Fudêncio";
+        estudante.age = 10;
+        estudante.gender = 'M';
+        System.out.println(estudante.name);
+        System.out.println(estudante.age);
+        System.out.println(estudante.gender);
+    }
+}
